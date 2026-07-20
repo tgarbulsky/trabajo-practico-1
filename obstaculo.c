7acbb64db66576098f9bbb01a9535227b0875b75
vcalderon-oss <vcalderon@fi.uba.ar>
2026-07-20 01:44:59 -0300

    Refactor crear_obstaculo to use 'posicion' array

--- obstaculo.c
+++ obstaculo.c
@@ -7,9 +7,9 @@ cuerpo_t* crear_obstaculo(){
         return NULL;
     }
     obstaculo->bloque=random_int(CUBO1, PIRAMIDE3);
-    obstaculo->pos[0]=random_float(-150, 150);
-    obstaculo->pos[1]=random_float(-150, 150);
-    obstaculo->pos[2]=0;
+    obstaculo->posicion[0]=random_float(-150, 150);
+    obstaculo->posicion[1]=random_float(-150, 150);
+    obstaculo->posicion[2]=0;
     obstaculo->angz=random_float(-M_PI, M_PI);
     return obstaculo;
 }
