c495533448bd73407772f9e4d2b1d909cbc18dd5
vcalderon-oss <vcalderon@fi.uba.ar>
2026-07-20 01:06:10 -0300

    Refactor pila.c for code consistency and clarity

--- pila.c
+++ pila.c
@@ -1,7 +1,5 @@
 #include "pila.h"
 #include <stdlib.h>
-#include <stdbool.h>
-#include <stdio.h>
 
 typedef struct nodo {
     void *dato;
@@ -14,25 +12,28 @@ struct pila {
 
 static nodo_t *crear_nodo(void *dato, nodo_t *sig) {
     nodo_t *nodo = malloc(sizeof(nodo_t));
-    if (nodo == NULL) return NULL;
+    if(nodo == NULL) return NULL;
+
     nodo->dato = dato;
     nodo->sig = sig;
+
     return nodo;
 }
 
 pila_t *pila_crear() {
     pila_t *p = malloc(sizeof(pila_t));
-    if (p == NULL) return NULL;
+    if(p == NULL) return NULL;
+
     p->tope = NULL;
+
     return p;
 }
 
 void pila_destruir(pila_t *p, void (*destruir_dato)(void *)) {
-    while (!pila_esta_vacia(p)) {
+    while(!pila_esta_vacia(p)) {
         void *dato = pila_desapilar(p);
-        if (destruir_dato != NULL) {
+        if(destruir_dato != NULL)
             destruir_dato(dato);
-        }
     }
     free(p);
 }
@@ -42,22 +43,28 @@ bool pila_esta_vacia(const pila_t *p) {
 }
 
 void *pila_ver_tope(const pila_t *p) {
-    if (p->tope == NULL) return NULL;
+    if(p->tope == NULL) return NULL;
+
     return p->tope->dato;
 }
 
 bool pila_apilar(pila_t *p, void *dato) {
     nodo_t *n = crear_nodo(dato, p->tope);
-    if (n == NULL) return false;
+    if(n == NULL) return false;
+
     p->tope = n;
+
     return true;
 }
 
 void *pila_desapilar(pila_t *p) {
-    if (p->tope == NULL) return NULL;
-    nodo_t *aux = p->tope;
-    void *dato = aux->dato;
-    p->tope = aux->sig;
-    free(aux);
+    if(p->tope == NULL) return NULL;
+
+    nodo_t *n = p->tope;
+    void *dato = n->dato;
+
+    p->tope = n->sig;
+    free(n);
+
     return dato;
 }
