87a9cb782276bd455dcc676b242cda70828a463f
vcalderon-oss <vcalderon@fi.uba.ar>
2026-07-20 01:00:15 -0300

    Refactor misil.h by cleaning up includes and comments
    Removed unnecessary includes and comments from misil.h.

--- misil.h
+++ misil.h
@@ -1,14 +1,10 @@
 #ifndef MISIL_H
 #define MISIL_H
 
-#include <stdbool.h>
 #include "mundo.h"
-#include "tanque.h"
 #include "lista.h"
 
-// Mueve el misil según su ángulo. 
-// Devuelve false si impacta contra un obstáculo o tanque.
-// Si impacta al tanque enemigo, pone kill en true.
+// comportamiento del misil
 bool misil_mover(cuerpo_t* misil, lista_t* obstaculos, tanque_t* otro_tanque, bool* kill);
 
 #endif
